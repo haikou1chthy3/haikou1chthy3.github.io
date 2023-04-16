87710490f50f8d75e6ac3f44f780db5f9d3dd1e3
Haikou1chthy3 <Haikou1chthy3@gmail.com>
2023-04-16 16:13:08 +0800

    test bundle

---
+++ dist/_nuxt/app.config.8eaad369.js
@@ -0,0 +1,1 @@
+import{a as n}from"./defu.72e658dd.js";const i={};n(i);
---
+++ dist/_nuxt/main.328d69a0.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./layout.16e3e0cb.js";import{_ as o,l as a,m as r,q as e,Y as c}from"./_plugin-vue_export-helper.1457150d.js";import"./nuxt.9babd985.js";import"./utils.861a55d6.js";const m={};function _(s,i){const n=t;return a(),r(c,null,[e(n,{name:"sub-container"}),e(n,{name:"main-container"}),e(n,{name:"sub-container"})],64)}const x=o(m,[["render",_]]);export{x as default};
